746fb00e5946762d51d45ecdc70ee848fcefb4a7
folaulau <folaukaveinga@gmail.com>
2022-09-02 17:20:21 -0600

    Update SushiRestaurantApiSpringbootApplicationTests.java

--- src/test/java/com/sushi/api/SushiRestaurantApiSpringbootApplicationTests.java
+++ src/test/java/com/sushi/api/SushiRestaurantApiSpringbootApplicationTests.java
@@ -1,13 +1,16 @@
 package com.sushi.api;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SushiRestaurantApiSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+  @Test
+  void contextLoads() {
+    String hello = "hello";
+    assertThat(hello).isNotNull().isEqualTo("hello");
+  }
 
 }

02751a3147d02072321170ebd87e82aaefe1a6eb
folaulau <folaukaveinga@gmail.com>
2022-09-17 21:28:40 -0600

    Update SecurityConfig.java

--- src/main/java/com/sushi/api/config/SecurityConfig.java
+++ src/main/java/com/sushi/api/config/SecurityConfig.java
@@ -87,6 +87,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         // handler access denied calls
         http.exceptionHandling().accessDeniedHandler(customAcccessDeniedHandler);
         
+        http.cors();
         // @formatter:on
     }
 
